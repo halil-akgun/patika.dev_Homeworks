680b0244de034b9348645b3d6e4b99feadf71fab
halil <halilakgun0@gmail.com>
2023-04-23 00:37:14 +0300

    java39_EmployeeRunner

---
+++ src/main/java/java101/java39_Employee/Employee.java
@@ -0,0 +1,47 @@
+package java101.java39_Employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private double salary;
+    private int workHours;
+    private int hireYear;
+
+
+    public double tax() {
+        return salary < 1000 ? 0 : salary * 0.03;
+    }
+
+    public int bonus() {
+        return workHours > 40 ? (workHours - 40) * 30 : 0;
+    }
+
+    public double raiseSalary() {
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear - hireYear < 10) return salary * 0.05;
+        else if (currentYear - hireYear < 20) return salary * 0.10;
+        else return salary * 0.15;
+    }
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Name\t\t: " + name + '\n' +
+                "Salary\t\t: " + salary + '\n' +
+                "Work Hours\t: " + workHours + '\n' +
+                "Hire Year\t: " + hireYear + '\n' +
+                "Tax\t\t\t: " + tax() + '\n' +
+                "Bonus\t\t: " + bonus() + '\n' +
+                "Raise Salary: " + raiseSalary() + '\n' +
+                "Salary with Tax and Bonus: " + (salary - tax() + bonus()) + '\n' +
+                "Total Salary: " + (salary - tax() + bonus() + raiseSalary());
+    }
+}
